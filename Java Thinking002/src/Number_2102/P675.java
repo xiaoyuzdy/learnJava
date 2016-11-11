f99e7b24ec9cead8d8aeb684a9f63271572b37d7
zcczdy <1104503035@qq.com>
2016-11-11 18:13:52 +0800

    16/11/11

---
+++ Java Thinking002/src/Number_2102/P675.java
@@ -0,0 +1,74 @@
+package Number_2102;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Number {
+	private static int i;
+
+	// 产生偶数
+	public static int next() {
+		i++;
+		i++;
+		return i;
+	}
+
+	/**
+	 * 加上 synchronized关键字 则当一个线程调用该方法时另一个线程被阻塞
+	 */
+	// public synchronized static int next() {
+	// i++;
+	// i++;
+	// return i;
+	// }
+
+}
+
+class A implements Runnable {
+	private boolean ok = false;
+
+	public void run() {
+		int t = 0;
+		while (true) {
+			t = Number.next();
+			// 如果获取的不是偶数就停止
+			if (t % 2 != 0) {
+				System.out.println(t + "  error");
+				ok = true;
+				System.exit(0);
+			}
+		}
+	}
+
+	public boolean isOk() {
+		return ok;
+	}
+
+}
+
+class B implements Runnable {
+	private A a = new A();
+
+	public void run() {
+		while (true) {
+			Number.next();
+			if (a.isOk())
+				System.exit(0);
+		}
+	}
+
+}
+
+public class P675 {
+	public static void main(String[] args) {
+		ExecutorService service = Executors.newCachedThreadPool();
+		service.execute(new A());
+		service.execute(new B());
+		service.shutdown();
+
+		/**
+		 * 结果： xxxx(一个奇数) error
+		 */
+	}
+
+}
